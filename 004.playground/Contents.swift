a2987d9dd6855f8b6de713e5d3c3698960c900d2
xiongbk <istud.k@gmail.com>
2023-07-13 14:44:13 +0800

    day 4

---
+++ 004.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+//  type inference or type annotation
+// 类型声明 类型推导
+// golden rule: Swift must at all times know what data types your constants and variables contain.
+
+let names = ["A","B","c"]
+print(names[2])
