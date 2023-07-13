0b1ac3f9d7b467ee65e691065a5664dbf7fed5ef
xiongbk <istud.k@gmail.com>
2023-07-13 17:33:57 +0800

    day 5

---
+++ 005.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Cocoa
+
+let ourName = "DA"
+let friendName = "AD"
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName{
+    print("It's \(friendName) vs \(ourName)")
+}
+
+
+let country = "Canada"
+
+if country == "Australia"{
+    print("G'day")
+}
+
+let name = "Taylor Swift"
+if name != "Anonymous" {
+    print("welcome,\(name)")
+}
+
+// isEmpty > cout 
+if name.isEmpty{
+    print(name)
+}
