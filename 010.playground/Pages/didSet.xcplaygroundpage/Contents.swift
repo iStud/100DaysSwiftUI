b1197bf8057d4dd88c89a133c37ec9951a9abc89
xiongbk <istud.k@gmail.com>
2023-07-19 09:37:50 +0800

    day 10

--- 010.playground/Pages/didSet.xcplaygroundpage/Contents.swift
+++ 010.playground/Pages/didSet.xcplaygroundpage/Contents.swift
@@ -1,7 +1,31 @@
-//: [Previous](@previous)
 
-import Foundation
+//a didSet observer to run when the property just changed,
+//and a willSet observer to run before the property changed.
 
-var greeting = "Hello, playground"
+struct App {
+    
+    var contacts = [String](){
+        
+        willSet{
+            print("cuuret value \(contacts)")
+            print("new value \(newValue)")
+        }
+        
+        didSet {
+            
+            print("there are now \(contacts.count) contacts")
+            print("old value was \(oldValue)")
+        }
+        
+    }
+}
 
-//: [Next](@next)
+//  Regardless of which you choose, please try to avoid putting too much work into property observers
+
+var app = App()
+app.contacts.append("A")
+print("--------")
+app.contacts.append("B")
+
+
+
