c53a97d7dc7b6ac93f92170cbbaa786ae8192954
xiongbk <istud.k@gmail.com>
2023-07-26 10:43:50 +0800

    day 18

--- 018/WeSplit/WeSplit/ContentView.swift
+++ 018/WeSplit/WeSplit/ContentView.swift
@@ -12,9 +12,9 @@ struct ContentView: View {
     @FocusState private var amountIsFocused: Bool
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
+    @State private var tipPercentage = 10
     let tipPercentages = [10,15,20,25,0]
-    var totalPerPerson:Double {
+    var totalPerPerson:[Double] {
         
         let peopleCount = Double(numberOfPeople+2)
         let tipSection = Double(tipPercentage)
@@ -22,8 +22,13 @@ struct ContentView: View {
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
         
-        return amountPerPerson
+        return [amountPerPerson,grandTotal]
     }
+    
+    let localCurrency:FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currency?.identifier ?? "USD")
+    
+
+    
     
     var body: some View {
         NavigationView{
@@ -42,17 +47,27 @@ struct ContentView: View {
                 Section {
                    
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages,id:\.self) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+//                    .pickerStyle(.segmented)
                 }header: {
                     Text("How much tip do you want to leave?")
                 }
                 
+                Section{
+
+                    Text(totalPerPerson[0], format:localCurrency)
+                }header: {
+                    Text("Total Amount")
+                }
+                
                 Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(totalPerPerson[1], format:localCurrency)
+                    
+                }header: {
+                    Text("Amount per person")
                 }
                 
 
