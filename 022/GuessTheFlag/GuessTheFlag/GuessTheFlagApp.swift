398514331c955766646a35a0dda0aa0a242dd095
xiongbk <istud.k@gmail.com>
2023-07-28 16:05:10 +0800

    day 22

---
+++ 022/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by xbk on 2023/7/27.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
