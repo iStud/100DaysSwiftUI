068a0abfc23b9ed8c19b995b407fa87f5f37e760
xiongbk <istud.k@gmail.com>
2023-08-02 11:15:55 +0800

    day 27

---
+++ 027/Sleep/Sleep/SleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepApp.swift
+//  Sleep
+//
+//  Created by xbk on 2023/8/2.
+//
+
+import SwiftUI
+
+@main
+struct SleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
