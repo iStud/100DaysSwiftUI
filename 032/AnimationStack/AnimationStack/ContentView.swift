855aad1bc14889b77a5e42151ba71b48cbc9d61b
xiongbk <istud.k@gmail.com>
2023-08-07 16:53:09 +0800

    day 32

--- 032/AnimationStack/AnimationStack/ContentView.swift
+++ 032/AnimationStack/AnimationStack/ContentView.swift
@@ -10,22 +10,35 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var enabled = false
+    @State private var animationAmount = 1.0
     
     
     var body: some View {
 
-        Button("Tap Me") {
-            enabled.toggle()
-        }
-        .frame(width: 200,height: 200)
-        .background(enabled ? .blue : .yellow)
-        .animation(nil, value: enabled)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+        print(animationAmount)
         
+        return VStack {
+            
+            Stepper("Scale amount",value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3,autoreverses: true)
+            ),in: 1...10)
+            Spacer()
+            
+            Button("Tap me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            
+        }
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
