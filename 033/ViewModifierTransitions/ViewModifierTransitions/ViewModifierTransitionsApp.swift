6eebc7bbd7e9b16fbe721ecfaaac1d5fe275b7e0
xiongbk <istud.k@gmail.com>
2023-08-09 16:58:20 +0800

    day 33

---
+++ 033/ViewModifierTransitions/ViewModifierTransitions/ViewModifierTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifierTransitionsApp.swift
+//  ViewModifierTransitions
+//
+//  Created by xbk on 2023/8/9.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifierTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
