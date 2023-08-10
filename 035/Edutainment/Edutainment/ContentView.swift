b944848da278523ef61f677733dfc52ef3980d68
xiongbk <istud.k@gmail.com>
2023-08-10 17:28:29 +0800

    day 35

---
+++ 035/Edutainment/Edutainment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by xbk on 2023/8/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 035/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by xbk on 2023/8/10.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
