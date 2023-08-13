2df7eec7baf0258d555f1edf4934f81026b6fb9c
xbk <istud.k@gmail.com>
2023-08-13 22:46:06 +0800

    day 36 - onDelete()

--- 036/iExpense/iExpense/ContentView.swift
+++ 036/iExpense/iExpense/ContentView.swift
@@ -30,25 +30,69 @@ struct ContentView: View {
     
     @StateObject var user = User()
     @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            TextField("FirstName", text: $user.firstName)
-            TextField("LastName", text: $user.lastName)
-            
-            Button("Show sheet") {
-                showingSheet.toggle()
+        NavigationView{
+            VStack {
+                
+                Form{
+                    
+                    Section{
+                        Text("Your name is \(user.firstName) \(user.lastName)")
+                        TextField("FirstName", text: $user.firstName)
+                        TextField("LastName", text: $user.lastName)
+                    }header: {
+                        Text("@StateObject")
+                    }
+                    
+                    Section {
+                        Button("Show sheet") {
+                            showingSheet.toggle()
+                        }
+                        .sheet(isPresented: $showingSheet) {
+                            SecondView(name: "@twostraws!")
+                        }
+                    }header: {
+                        Text("Sheets")
+                    }
+                    
+                    Section {
+                        
+                        List {
+                            ForEach(numbers,id:\.self) {
+                                Text("Row \($0)")
+                            }
+                            .onDelete(perform: removeRows)
+                        }
+                        
+                        Button("Add Number") {
+                            
+                            numbers.append(currentNumber)
+                            currentNumber += 1
+                        }
+                        
+                    }header: {
+                        Text("onDelete()")
+                    }
+                    
+                }
+                
             }
-            .sheet(isPresented: $showingSheet) {
-                SecondView(name: "@twostraws!")
+            .navigationTitle("Day 36")
+            .toolbar {
+                EditButton()
             }
             
         }
-        .padding()
-
         
     }
+    
+    func removeRows(at offsets:IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
