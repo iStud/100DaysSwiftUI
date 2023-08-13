4fd307913406b15b14dacd9424c8ee8f9eb726ac
xbk <istud.k@gmail.com>
2023-08-13 20:22:31 +0800

    day 36

--- 036/iExpense/iExpense/ContentView.swift
+++ 036/iExpense/iExpense/ContentView.swift
@@ -13,17 +13,41 @@ class User:ObservableObject{
     @Published var lastName = "Baggins"
 }
 
+struct SecondView:View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    let name:String
+    var body: some View{
+        
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
+}
+
 struct ContentView: View {
     
     @StateObject var user = User()
+    @State private var showingSheet = false
     
     var body: some View {
         VStack {
             Text("Your name is \(user.firstName) \(user.lastName)")
             TextField("FirstName", text: $user.firstName)
             TextField("LastName", text: $user.lastName)
+            
+            Button("Show sheet") {
+                showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                SecondView(name: "@twostraws!")
+            }
+            
         }
         .padding()
+
+        
     }
 }
 
