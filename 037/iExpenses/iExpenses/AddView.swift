bedda0e8eebfa077672595560cdb1d85a07efae0
xiongbk <istud.k@gmail.com>
2023-08-14 16:23:28 +0800

    day 37

--- 037/iExpenses/iExpenses/AddView.swift
+++ 037/iExpenses/iExpenses/AddView.swift
@@ -15,6 +15,7 @@ struct AddView: View {
     let types = ["Business","Personal"]
     
     @ObservedObject var expenses: Expense
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         NavigationView{
@@ -35,7 +36,7 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
-                    
+                    dismiss()
                 }
             }
         }
