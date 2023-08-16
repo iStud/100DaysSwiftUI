6bec2342135f6070504de0dec0417efa88ba49c8
xbk <istud.k@gmail.com>
2023-08-16 23:03:56 +0800

    day 39 Codable

---
+++ 039/Codable/Codable/CodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableApp.swift
+//  Codable
+//
+//  Created by bk.xiong on 2023/8/16.
+//
+
+import SwiftUI
+
+@main
+struct CodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 039/Codable/Codable/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Codable
+//
+//  Created by bk.xiong on 2023/8/16.
+//
+
+import SwiftUI
+
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data){
+                print(user.address.street)
+            }
+                
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
